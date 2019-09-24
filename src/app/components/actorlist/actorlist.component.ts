04e724fd012bb6d8940c6b54b4335603378d9026
IBM931-PC0BQ513\Manish <manisk08@in.ibm.com>
2019-09-24 16:03:33 +0530

    Shallow Copy Problem Solved

--- src/app/components/actorlist/actorlist.component.ts
+++ src/app/components/actorlist/actorlist.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import { THIS_EXPR } from "@angular/compiler/src/output/output_ast";
+import { worker } from "cluster";
 
 @Component({
   selector: "app-actorlist",
@@ -7,22 +9,55 @@ import { Component, OnInit } from "@angular/core";
 })
 export class ActorlistComponent implements OnInit {
   actorList;
+  selectedIndex: number;
+  tempActor;
   constructor() {}
 
   ngOnInit() {
     this.actorList = [
       {
-        name: "Rajnokant",
-        Rating: 10
+        name: "Amitabh",
+        Rating: 70
       },
       {
-        name: "Rajnokant",
-        Rating: 10
+        name: "Rajnikant",
+        Rating: 70
       }
     ];
+
+    this.selectedIndex = -1;
+    this.tempActor = null;
   }
 
-  deleteActor(index) {
+  deleteActor(index: number) {
     this.actorList.splice(index, 1);
+  }
+  editActor(index: number) {
+    this.selectedIndex = index;
+    this.tempActor = this.actorList[index];
+    //Do not use this
+    //shallow copy Problem
+
+    //Solution - DEEP COPY
+
+    //Approach 1(Works But not Recommended)
+    // this.tempActor = {
+    //   name: this.actorList[index].name,
+    //   Rating: this.actorList[index].Rating
+    // };
+
+    // Approch 2 (May not Work worker, not Recommended)
+    // this.tempActor = Object.assign({}, this.actorList[index]);
+    //Solve problem for one level on , embeded object will cause problem
+    // Approach 3(Best approch ,Always recommneded)
+
+    this.tempActor = JSON.parse(JSON.stringify(this.actorList[index]));
+  }
+  saveActor(index: number) {
+    this.selectedIndex = -1;
+  }
+  cancelActor(index: number) {
+    this.actorList[index] = this.tempActor;
+    this.selectedIndex = -1;
   }
 }
