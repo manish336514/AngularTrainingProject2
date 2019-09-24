cce3d3d0d4bee1f020e535a8a7e8ecdab92c3f0b
IBM931-PC0BQ513\Manish <manisk08@in.ibm.com>
2019-09-24 15:20:24 +0530

    [(ngModel)] for Two way Binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { ActorlistComponent } from "./components/actorlist/actorlist.component";
-import { MydemoComponent } from './components/mydemo/mydemo.component';
+import { MydemoComponent } from "./components/mydemo/mydemo.component";
 
 @NgModule({
   declarations: [AppComponent, ActorlistComponent, MydemoComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
